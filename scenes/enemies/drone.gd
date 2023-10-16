05c1450cabc20f86240ed149e85d25264b54665c
Arshia Shafiei <arshiashafiei03@gmail.com>
2023-10-16 15:38:45 +0330

    Make laser disappear after hitting objects

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 200
 func _process(_delta):
-    var direction = Vector2.RIGHT
-    velocity = SPEED * direction
-    move_and_slide()
-    
+	var direction = Vector2.RIGHT
+	velocity = SPEED * direction
+	move_and_slide()
+	
+
+func hit():
+	print("hit")
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -7,3 +7,9 @@ var direction: Vector2
 
 func _process(delta):
 	position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
